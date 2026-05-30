deec2a393b6e396315b1579222b79e20a47514ff
Madhur Ahuja <ahujamadhur2016@gmail.com>
2026-05-30 17:17:57 +0530

    Improve homepage UI

--- app.py
+++ app.py
@@ -2,15 +2,49 @@ from fastapi import FastAPI, HTTPException
 from etl import run_etl
 import pandas as pd
 from sqlalchemy import create_engine
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def home():
-    return {
-        "message": "University ETL API"
-    }
+    return """
+        <html>
+        <head>
+            <title>University ETL App</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    text-align: center;
+                    margin-top: 100px;
+                    background-color: #f4f4f4;
+                }
+                h1 {
+                    color: #2c3e50;
+                }
+                p {
+                    color: #555;
+                }
+                a {
+                    display: inline-block;
+                    margin-top: 20px;
+                    padding: 10px 20px;
+                    background: #007bff;
+                    color: white;
+                    text-decoration: none;
+                    border-radius: 5px;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>🎓 University ETL App</h1>
+            <p>Extract, Transform, and Load university data from a public API.</p>
+
+            <a href="/docs">Open API Documentation</a>
+        </body>
+    </html>
+    """
 
 
 @app.post("/run-etl")
